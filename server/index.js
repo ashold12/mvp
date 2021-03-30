c4c89aeb5092183026f1bc50e8e467c257f24dda
AlexShold <ashold12@gmail.com>
2021-03-30 14:28:20 -0500

    request to foreign api working

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
+
+const TOKEN = require('../config.js');
 
 const app = express();
 const port = 3000;
@@ -8,10 +11,26 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
 app.get('/concerts', (req, res) => {
-  //expect city parameter as string
+  //expect city in query as string
   console.log(req.query)
-  let thing = req.query
-  res.send(thing)
+  if (!('city' in req.query) || !('state' in req.query)) {
+    res.status(400).send('invalid request')
+  }
+  const { city, region } = req.query
+
+  const parameters = {
+    url: `https://api.predicthq.com/v1/places/?q=${city}`,
+    headers: {
+      Authorization: `Bearer ${TOKEN.TOKEN}`,
+    },
+  }
+  axios.get(parameters.url, {headers: parameters.headers}).then((response) => {
+    res.send(response.data)
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send(err)
+  })
 })
 
 
